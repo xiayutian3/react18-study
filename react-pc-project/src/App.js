e9f67054746001b6ad0439c606089d86cf22fa1d
1127071993@qq.com <aini2009626>
2023-06-28 12:06:18 +0800

    项目打包-路由懒加载

--- react-pc-project/src/App.js
+++ react-pc-project/src/App.js
@@ -1,43 +1,61 @@
 // 导入路由
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-// 导入页面组件
-// import Login from "./pages/Login";
-import Login from "@/pages/Login";
-import Layout from "./pages/Layout";
+
 // 路由鉴权-高阶组件
 import { AuthRoute } from "@/components/AuthRoute";
 
 import "./App.css";
-import Home from "./pages/Home";
-import Article from "./pages/Article";
-import Publish from "./pages/Publish";
+
+// 导入页面组件
+// import Login from "./pages/Login";
+// 导入必要组件
+import { lazy, Suspense } from "react";
 
 // 解决在非组件外使用router跳转问题
-import { HistoryRouter, history } from './utils/history'
+import { HistoryRouter, history } from "./utils/history";
+// 按需导入路由组件
+const Login = lazy(() => import("./pages/Login"));
+const Layout = lazy(() => import("./pages/Layout"));
+const Home = lazy(() => import("./pages/Home"));
+const Article = lazy(() => import("./pages/Article"));
+const Publish = lazy(() => import("./pages/Publish"));
 
 // 配置路由规则
 function App() {
   return (
     <HistoryRouter history={history}>
-      <div className="App">
-        <Routes>
-          {/* 需要鉴权的路由 */}
-          <Route
-            path="/"
-            element={
-              <AuthRoute>
-                <Layout />
-              </AuthRoute>
-            }
+      <Suspense
+        fallback={
+          <div
+            style={{
+              textAlign: "center",
+              marginTop: 200,
+            }}
           >
-            <Route index element={<Home />} />
-            <Route path="article" element={<Article />} />
-            <Route path="publish" element={<Publish />} />
-          </Route>
-          {/* 不需要鉴权的路由 */}
-          <Route path="/login" element={<Login />} />
-        </Routes>
-      </div>
+            loading...
+          </div>
+        }
+      >
+        <div className="App">
+          <Routes>
+            {/* 需要鉴权的路由 */}
+            <Route
+              path="/"
+              element={
+                <AuthRoute>
+                  <Layout />
+                </AuthRoute>
+              }
+            >
+              <Route index element={<Home />} />
+              <Route path="article" element={<Article />} />
+              <Route path="publish" element={<Publish />} />
+            </Route>
+            {/* 不需要鉴权的路由 */}
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </div>
+      </Suspense>
     </HistoryRouter>
   );
 }
--- react-pc-project/src/pages/Publish/index.js
+++ react-pc-project/src/pages/Publish/index.js
@@ -8,22 +8,25 @@ import {
   Upload,
   Space,
   Select,
+  message,
 } from "antd";
 import { PlusOutlined } from "@ant-design/icons";
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 import "./index.scss";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 import { useStore } from "@/store";
 import { observer } from "mobx-react-lite";
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
+import { http } from "@/utils";
 
 const { Option } = Select;
 
 const Publish = () => {
   const { channelStore } = useStore();
-  console.log('channelStore: ', channelStore);
 
+  // 1. 声明一个暂存仓库
+  const fileListRef = useRef([]);
   const [fileList, setFileList] = useState([]);
   // 上传成功回调
   const onUploadChange = (info) => {
@@ -36,7 +39,84 @@ const Publish = () => {
       return file;
     });
     setFileList(fileList);
+    // 2. 上传图片时，将所有图片存储到 ref 中
+    fileListRef.current = fileList;
   };
+
+  // 图片切换显示
+  const [imgCount, setImgCount] = useState(1);
+
+  const changeType = (e) => {
+    const count = e.target.value;
+    setImgCount(count);
+
+    if (count === 1) {
+      // 单图，只展示第一张
+      const firstImg = fileListRef.current[0];
+      setFileList(!firstImg ? [] : [firstImg]);
+    } else if (count === 3) {
+      // 三图，展示所有图片
+      setFileList(fileListRef.current);
+    }
+  };
+
+  // 发布文章
+  const onFinish = async (values) => {
+    // 数据的二次处理 重点是处理cover字段
+    const { channel_id, content, title, type } = values;
+    const params = {
+      channel_id,
+      content,
+      title,
+      type,
+      cover: {
+        type: type,
+        images: fileList.map((item) => item.url),
+      },
+    };
+    try {
+      if (articleId) {
+        // 编辑
+        await http.put(`/mp/articles/${articleId}?draft=false`, params);
+      } else {
+        // 新增
+        await http.post("/mp/articles?draft=false", params);
+      }
+    } catch (error) {
+      console.log("error: ", error);
+    }
+    // 提示用户
+    navigator("/article");
+    message.success(`${articleId}` ? "更新成功" : "发布成功");
+  };
+
+  // 回显编辑文章
+  const [params] = useSearchParams();
+  const articleId = params.get("id");
+  // 数据回填  id调用接口  1.表单回填 2.暂存列表 3.Upload组件fileList
+  const [form] = Form.useForm();
+
+  useEffect(() => {
+    async function getArticle() {
+      try {
+        const res = await http.get(`/mp/articles/${articleId}`);
+        const { cover, ...formValue } = res.data;
+        // 动态设置表单数据
+        form.setFieldsValue({ ...formValue, type: cover.type });
+        // 格式化封面图片数据
+        const imageList = cover.images.map((url) => ({ url }));
+        setFileList(imageList);
+        setImgCount(cover.type);
+        fileListRef.current = imageList;
+      } catch (error) {
+        console.log("error: ", error);
+      }
+    }
+    if (articleId) {
+      // 拉取数据回显
+      getArticle();
+    }
+  }, [articleId, form]); //事实上只需要发送一次及就可以了，填上id是为了解决警告问题
 
   return (
     <div className="publish">
@@ -46,7 +126,10 @@ const Publish = () => {
             <Breadcrumb.Item>
               <Link to="/">首页</Link>
             </Breadcrumb.Item>
-            <Breadcrumb.Item>发布文章</Breadcrumb.Item>
+            <Breadcrumb.Item>
+              {" "}
+              {articleId ? "修改文章" : "发布文章"}
+            </Breadcrumb.Item>
           </Breadcrumb>
         }
       >
@@ -54,6 +137,8 @@ const Publish = () => {
           labelCol={{ span: 4 }}
           wrapperCol={{ span: 16 }}
           initialValues={{ type: 1 }}
+          onFinish={onFinish}
+          form={form}
         >
           <Form.Item
             label="标题"
@@ -78,25 +163,29 @@ const Publish = () => {
 
           <Form.Item label="封面">
             <Form.Item name="type">
-              <Radio.Group>
+              <Radio.Group onChange={changeType}>
                 <Radio value={1}>单图</Radio>
                 <Radio value={3}>三图</Radio>
                 <Radio value={0}>无图</Radio>
               </Radio.Group>
             </Form.Item>
-            <Upload
-              name="image"
-              listType="picture-card"
-              className="avatar-uploader"
-              showUploadList
-              action="http://geek.itheima.net/v1_0/upload"
-              fileList={fileList}
-              onChange={onUploadChange}
-            >
-              <div style={{ marginTop: 8 }}>
-                <PlusOutlined />
-              </div>
-            </Upload>
+            {imgCount > 0 && (
+              <Upload
+                name="image"
+                listType="picture-card"
+                className="avatar-uploader"
+                showUploadList
+                action="http://geek.itheima.net/v1_0/upload"
+                fileList={fileList}
+                onChange={onUploadChange}
+                maxCount={imgCount}
+                multiple={imgCount > 1}
+              >
+                <div style={{ marginTop: 8 }}>
+                  <PlusOutlined />
+                </div>
+              </Upload>
+            )}
           </Form.Item>
           <Form.Item
             label="内容"
@@ -113,7 +202,7 @@ const Publish = () => {
           <Form.Item wrapperCol={{ offset: 4 }}>
             <Space>
               <Button size="large" type="primary" htmlType="submit">
-                发布文章
+                {articleId ? "修改文章" : "发布文章"}
               </Button>
             </Space>
           </Form.Item>
